c69b4e71ef660891b7d06fdf53b14165e015b6ba
OrfeasZ <orfeaz@gmail.com>
2016-09-14 22:20:48 +0300

    Added more benchmarks for member variable getting and setting, evaluating more complex scenarios.
    Disabled plain C benchmarks for member variables until they are properly re-written.

--- lua bench/src/basic.i
+++ lua bench/src/basic.i
@@ -13,6 +13,71 @@ struct basic {
 	void set(int x);
 };
 
+struct basic_var {
+	int var0;
+	int var1;
+	int var2;
+	int var3;
+	int var4;
+
+	basic_var();
+};
+
+struct basic_var_complex {
+	int var0;
+	int var1;
+	int var2;
+	int var3;
+	int var4;
+	int var5;
+	int var6;
+	int var7;
+	int var8;
+	int var9;
+	int var10;
+	int var11;
+	int var12;
+	int var13;
+	int var14;
+	int var15;
+	int var16;
+	int var17;
+	int var18;
+	int var19;
+	int var20;
+	int var21;
+	int var22;
+	int var23;
+	int var24;
+	int var25;
+	int var26;
+	int var27;
+	int var28;
+	int var29;
+	int var30;
+	int var31;
+	int var32;
+	int var33;
+	int var34;
+	int var35;
+	int var36;
+	int var37;
+	int var38;
+	int var39;
+	int var40;
+	int var41;
+	int var42;
+	int var43;
+	int var44;
+	int var45;
+	int var46;
+	int var47;
+	int var48;
+	int var49;
+
+	basic_var_complex();
+};
+
 struct complex_base_a {
 	int a;
 
--- lua bench/src/luabind.cpp
+++ lua bench/src/luabind.cpp
@@ -192,12 +192,212 @@ namespace lb {
 		luabind::module(L)[
 			luabind::class_<basic>("basic")
 				.def(luabind::constructor<>())
-				.def_nonconst("set", &basic::set)
-				.def_const("get", &basic::get)
-				.def_readwrite("var", &basic::var)
+				.def_readwrite("var0", &basic::var)
+				.def_readwrite("var1", &basic::var)
+				.def_readwrite("var2", &basic::var)
+				.def_readwrite("var3", &basic::var)
+				.def_readwrite("var4", &basic::var)
 		];
 		lua_do_or_die(L, "b = basic()");
-		std::string code = repeated_code("b.var = i\nx = b.var");
+		std::string code = repeated_code(
+			"b.var0 = i\n"
+			"x = b.var0\n"
+			"b.var4 = i\n"
+			"x = b.var4\n"
+			"b.var1 = i\n"
+			"x = b.var1\n"
+			"b.var3 = i\n"
+			"x = b.var3\n"
+			"b.var2 = i\n"
+			"x = b.var2\n"
+		);
+		meter.measure([&]() {
+			lua_do_or_die(L, code.c_str());
+		});
+	}
+
+	void luabind_member_variable_complex_measure(nonius::chronometer& meter) {
+		lua_State* L = luaL_newstate();
+		lua_atpanic(L, panic_throw);
+
+		luaL_openlibs(L);
+		luabind::open(L);
+		luabind::module(L)[
+			luabind::class_<basic>("basic")
+				.def(luabind::constructor<>())
+				.def_readwrite("var0", &basic::var)
+				.def_readwrite("var1", &basic::var)
+				.def_readwrite("var2", &basic::var)
+				.def_readwrite("var3", &basic::var)
+				.def_readwrite("var4", &basic::var)
+				.def_readwrite("var5", &basic::var)
+				.def_readwrite("var6", &basic::var)
+				.def_readwrite("var7", &basic::var)
+				.def_readwrite("var8", &basic::var)
+				.def_readwrite("var9", &basic::var)
+				.def_readwrite("var10", &basic::var)
+				.def_readwrite("var11", &basic::var)
+				.def_readwrite("var12", &basic::var)
+				.def_readwrite("var13", &basic::var)
+				.def_readwrite("var14", &basic::var)
+				.def_readwrite("var15", &basic::var)
+				.def_readwrite("var16", &basic::var)
+				.def_readwrite("var17", &basic::var)
+				.def_readwrite("var18", &basic::var)
+				.def_readwrite("var19", &basic::var)
+				.def_readwrite("var20", &basic::var)
+				.def_readwrite("var21", &basic::var)
+				.def_readwrite("var22", &basic::var)
+				.def_readwrite("var23", &basic::var)
+				.def_readwrite("var24", &basic::var)
+				.def_readwrite("var25", &basic::var)
+				.def_readwrite("var26", &basic::var)
+				.def_readwrite("var27", &basic::var)
+				.def_readwrite("var28", &basic::var)
+				.def_readwrite("var29", &basic::var)
+				.def_readwrite("var30", &basic::var)
+				.def_readwrite("var31", &basic::var)
+				.def_readwrite("var32", &basic::var)
+				.def_readwrite("var33", &basic::var)
+				.def_readwrite("var34", &basic::var)
+				.def_readwrite("var35", &basic::var)
+				.def_readwrite("var36", &basic::var)
+				.def_readwrite("var37", &basic::var)
+				.def_readwrite("var38", &basic::var)
+				.def_readwrite("var39", &basic::var)
+				.def_readwrite("var40", &basic::var)
+				.def_readwrite("var41", &basic::var)
+				.def_readwrite("var42", &basic::var)
+				.def_readwrite("var43", &basic::var)
+				.def_readwrite("var44", &basic::var)
+				.def_readwrite("var45", &basic::var)
+				.def_readwrite("var46", &basic::var)
+				.def_readwrite("var47", &basic::var)
+				.def_readwrite("var48", &basic::var)
+				.def_readwrite("var49", &basic::var)
+		];
+		lua_do_or_die(L, "b = basic()");
+		std::string code = repeated_code(
+			"b.var0 = i\nx = b.var0\n"
+			"b.var49 = i\nx = b.var49\n"
+			"b.var2 = i\nx = b.var2\n"
+			"b.var47 = i\nx = b.var47\n"
+			"b.var4 = i\nx = b.var4\n"
+			"b.var45 = i\nx = b.var45\n"
+			"b.var6 = i\nx = b.var6\n"
+			"b.var43 = i\nx = b.var43\n"
+			"b.var8 = i\nx = b.var8\n"
+			"b.var41 = i\nx = b.var41\n"
+			"b.var10 = i\nx = b.var10\n"
+			"b.var39 = i\nx = b.var39\n"
+			"b.var12 = i\nx = b.var12\n"
+			"b.var37 = i\nx = b.var37\n"
+			"b.var14 = i\nx = b.var14\n"
+			"b.var35 = i\nx = b.var35\n"
+			"b.var16 = i\nx = b.var16\n"
+			"b.var33 = i\nx = b.var33\n"
+			"b.var18 = i\nx = b.var18\n"
+			"b.var31 = i\nx = b.var31\n"
+			"b.var20 = i\nx = b.var20\n"
+			"b.var29 = i\nx = b.var29\n"
+			"b.var22 = i\nx = b.var22\n"
+			"b.var27 = i\nx = b.var27\n"
+			"b.var24 = i\nx = b.var24\n"
+			"b.var25 = i\nx = b.var25\n"
+			"b.var26 = i\nx = b.var26\n"
+			"b.var23 = i\nx = b.var23\n"
+			"b.var28 = i\nx = b.var28\n"
+			"b.var21 = i\nx = b.var21\n"
+			"b.var30 = i\nx = b.var30\n"
+			"b.var19 = i\nx = b.var19\n"
+			"b.var32 = i\nx = b.var32\n"
+			"b.var17 = i\nx = b.var17\n"
+			"b.var34 = i\nx = b.var34\n"
+			"b.var15 = i\nx = b.var15\n"
+			"b.var36 = i\nx = b.var36\n"
+			"b.var13 = i\nx = b.var13\n"
+			"b.var38 = i\nx = b.var38\n"
+			"b.var11 = i\nx = b.var11\n"
+			"b.var40 = i\nx = b.var40\n"
+			"b.var9 = i\nx = b.var9\n"
+			"b.var42 = i\nx = b.var42\n"
+			"b.var7 = i\nx = b.var7\n"
+			"b.var44 = i\nx = b.var44\n"
+			"b.var5 = i\nx = b.var5\n"
+			"b.var46 = i\nx = b.var46\n"
+			"b.var3 = i\nx = b.var3\n"
+			"b.var48 = i\nx = b.var48\n"
+			"b.var1 = i\nx = b.var1\n"
+		);
+		meter.measure([&]() {
+			lua_do_or_die(L, code.c_str());
+		});
+	}
+
+	void luabind_member_variable_last_measure(nonius::chronometer& meter) {
+		lua_State* L = luaL_newstate();
+		lua_atpanic(L, panic_throw);
+
+		luaL_openlibs(L);
+		luabind::open(L);
+		luabind::module(L)[
+			luabind::class_<basic>("basic")
+				.def(luabind::constructor<>())
+				.def_readwrite("var0", &basic::var)
+				.def_readwrite("var1", &basic::var)
+				.def_readwrite("var2", &basic::var)
+				.def_readwrite("var3", &basic::var)
+				.def_readwrite("var4", &basic::var)
+				.def_readwrite("var5", &basic::var)
+				.def_readwrite("var6", &basic::var)
+				.def_readwrite("var7", &basic::var)
+				.def_readwrite("var8", &basic::var)
+				.def_readwrite("var9", &basic::var)
+				.def_readwrite("var10", &basic::var)
+				.def_readwrite("var11", &basic::var)
+				.def_readwrite("var12", &basic::var)
+				.def_readwrite("var13", &basic::var)
+				.def_readwrite("var14", &basic::var)
+				.def_readwrite("var15", &basic::var)
+				.def_readwrite("var16", &basic::var)
+				.def_readwrite("var17", &basic::var)
+				.def_readwrite("var18", &basic::var)
+				.def_readwrite("var19", &basic::var)
+				.def_readwrite("var20", &basic::var)
+				.def_readwrite("var21", &basic::var)
+				.def_readwrite("var22", &basic::var)
+				.def_readwrite("var23", &basic::var)
+				.def_readwrite("var24", &basic::var)
+				.def_readwrite("var25", &basic::var)
+				.def_readwrite("var26", &basic::var)
+				.def_readwrite("var27", &basic::var)
+				.def_readwrite("var28", &basic::var)
+				.def_readwrite("var29", &basic::var)
+				.def_readwrite("var30", &basic::var)
+				.def_readwrite("var31", &basic::var)
+				.def_readwrite("var32", &basic::var)
+				.def_readwrite("var33", &basic::var)
+				.def_readwrite("var34", &basic::var)
+				.def_readwrite("var35", &basic::var)
+				.def_readwrite("var36", &basic::var)
+				.def_readwrite("var37", &basic::var)
+				.def_readwrite("var38", &basic::var)
+				.def_readwrite("var39", &basic::var)
+				.def_readwrite("var40", &basic::var)
+				.def_readwrite("var41", &basic::var)
+				.def_readwrite("var42", &basic::var)
+				.def_readwrite("var43", &basic::var)
+				.def_readwrite("var44", &basic::var)
+				.def_readwrite("var45", &basic::var)
+				.def_readwrite("var46", &basic::var)
+				.def_readwrite("var47", &basic::var)
+				.def_readwrite("var48", &basic::var)
+				.def_readwrite("var49", &basic::var)
+		];
+		lua_do_or_die(L, "b = basic()");
+		std::string code = repeated_code(
+			"b.var49 = i\nx = b.var49\n"
+		);
 		meter.measure([&]() {
 			lua_do_or_die(L, code.c_str());
 		});
